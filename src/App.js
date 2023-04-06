e82e42a929ec7c8dc82d715c97e4c988ff37a32f
PitRovin <rovithispetros@gmail.com>
2023-04-06 21:09:12 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Navigation from './components/navigation';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <Switch>
+
+      <Route path="/search" component={SearchPage} />
+
+      <Route path="/pet-details-not-found" component={PetDetailsNotFound} />
+      
+      <Route path="/:type/:id" component={PetDetailsPage} />
+
+      <Route path="/:type?" component={HomePage} />
+      
+      </Switch>
+
+
+    </Router>
+  );
+
+}
+
+export default App;
+
